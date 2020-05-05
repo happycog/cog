6efcfe035423ae38075d2beb44743634ac952823
Jeremy Gimbel <jeremy@happycog.com>
2020-05-05 13:13:24 -0400

    add colors.sh

---
+++ tools/colors.sh
@@ -0,0 +1,8 @@
+# Colors
+export RED='\033[0;91m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export MAGENTA='\033[0;35m'
+export BOLD='\033[0;22m'
+export NC='\033[0m' # No Color
